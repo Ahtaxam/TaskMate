f7d4555666bf27dda639b899cba6a7ca69b3a649
ahtasham <ahtaxam03@gmail.com>
2023-04-08 23:16:32 +0500

    feat: add component for single task

---
+++ src/component/SingleTask.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import  {VscListUnordered}  from 'react-icons/vsc';
+
+
+function SingleTask({name , onclick , id}) {
+  return (
+    <div id='singleTask' >
+        <div id='singleTask_define'>
+        <VscListUnordered  id='singleTask_icon' />
+        <span id='singleTask_text' >{name}</span>
+        </div>
+        <div>
+        <button id='singleTask__delete' onClick={() => onclick(id)} >Delete</button>
+        </div>
+    </div>
+  )
+}
+
+export default SingleTask
