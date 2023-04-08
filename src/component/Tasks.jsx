589237ab05574233e96455ca4c232de7d5d76d05
ahtasham <ahtaxam03@gmail.com>
2023-04-08 23:21:10 +0500

    feat: add functionality

--- src/component/Tasks.jsx
+++ src/component/Tasks.jsx
@@ -1,17 +1,30 @@
-import React, { useMemo }  from 'react'
+import React  from 'react'
+import SingleTask from './SingleTask'
 
 
 
 function Tasks({tasklist , id}) {
 
+  const handleOnClick = (id) => {
+    console.log( id)
+  }
+
+
   return (
     <div id='tasks'>
       <span id='tasks__heading'>Active Tasks</span>
       <div id='activeTasks'>
-        
+        {
+          tasklist.map((obj, index) => (
+            <div key={index}>
+            <SingleTask name = {obj.task} onclick = {handleOnClick} id = {index} ></SingleTask>
+            </div>
+          )
+          )
+        }
       </div>
     </div>
   )
 }
 
-export default (Tasks)
+export default Tasks
