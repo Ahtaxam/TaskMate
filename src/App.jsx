b0909733b2222a5d108fedf97658241f31dc8dd0
ahtasham <ahtaxam03@gmail.com>
2023-04-09 08:49:33 +0500

    refactor: made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './component/Home'
-import Tasks from './component/Tasks'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <Home/>
-      <Tasks/>
     </div>
   )
 }
